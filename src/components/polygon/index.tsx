e6b43f38d0a9af16a9afa338dea2d0a1c7e3937d
Elijah Spina, PhD <ejspina@gmail.com>
2023-08-23 18:18:11 -0700

    Update index.tsx

--- src/components/polygon/index.tsx
+++ src/components/polygon/index.tsx
@@ -11,7 +11,7 @@ export function LinktreeBOS() {
     return (
         <Container>
             <VmComponent
-                src="opencann.near/widget/links"
+                src="opencann.near/widget/welcome"
                 props={{accountId: process.env.NEXT_PUBLIC_ACCOUNT_ID}}
             />
         </Container>
